95658b79c4e7d3f042def157ac91d23ed27cfc06
Jackie Yang <valkjsaaa@users.noreply.github.com>
2023-07-02 14:37:39 -0700

    Update ai.py

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -56,13 +56,4 @@ class AI:
 
 
 def fallback_model(model: str) -> str:
-    try:
-        openai.Model.retrieve(model)
-        return model
-    except openai.InvalidRequestError:
-        print(
-            f"Model {model} not available for provided API key. Reverting "
-            "to gpt-3.5-turbo. Sign up for the GPT-4 wait list here: "
-            "https://openai.com/waitlist/gpt-4-api\n"
-        )
-        return "gpt-3.5-turbo"
+    return model
